21fcf9147136eefb7e8750827342519266eb502f
Ian <emmanuelian866@gmail.com>
2026-07-01 14:22:13 +0100

    Google auth fix

--- public/config.js
+++ public/config.js
@@ -1,2 +1,2 @@
 // Public, non-secret config. The Google Client ID is safe to expose client-side.
-372327574308-ueppqjr12srvmp1delot79cvqvedjmj8.apps.googleusercontent.com;
+window.GOOGLE_CLIENT_ID = "372327574308-ueppqjr12srvmp1delot79cvqvedjmj8.apps.googleusercontent.com";
