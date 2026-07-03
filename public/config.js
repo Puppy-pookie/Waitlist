fe0c6c5b8d67e1d7135090913ca6a6d2a283b561
Ian <emmanuelian866@gmail.com>
2026-07-03 07:56:14 +0100

    Ui rework

--- public/config.js
+++ public/config.js
@@ -1,2 +1,2 @@
 // Public, non-secret config. The Google Client ID is safe to expose client-side.
-window.GOOGLE_CLIENT_ID = "372327574308-ueppqjr12srvmp1delot79cvqvedjmj8.apps.googleusercontent.com";
+window.GOOGLE_CLIENT_ID = "YOUR_GOOGLE_CLIENT_ID.apps.googleusercontent.com";
